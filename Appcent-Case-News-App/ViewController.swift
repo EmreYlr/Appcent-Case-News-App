c0ece774131934d7c2f412ed531d40ab3867c6ca
Emre Yeler <yeleremre@hotmail.com>
2024-05-09 12:18:53 +0300

    Initial commit

---
+++ Appcent-Case-News-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appcent-Case-News-App
+//
+//  Created by Emre on 9.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World!")
+    }
+
+
+}
+
